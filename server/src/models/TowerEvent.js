57c5bac5d0c2c0db37797ab71508757e0b131b8f
Zach Barnes <zachary.barnes.32@gmail.com>
2024-07-23 16:57:17 -0600

    created the TowerEventSchema in TowerEvent.js

---
+++ server/src/models/TowerEvent.js
@@ -0,0 +1,21 @@
+import { Schema } from "mongoose";
+
+
+export const TowerEventSchema = new Schema({
+    creatorId: { type: Schema.ObjectId, ref: 'Account', required: true },
+    name: { type: String, minlength: 3, maxlength: 50, required: true },
+    description: { type: String, minlength: 15, maxlength: 1000, required: true },
+    coverImg: { type: String, maxlength: 1000, required: true },
+    location: { type: String, minlength: 1, maxlength: 500, required: true }, 
+    capacity: { type: Number, min: 1, max: 5000, required: true },
+    startDate: { type: Date, required: true },
+    isCanceled: { type: Boolean, default: false, required: true },
+    type: { type: String, enum: ['concert', 'convention', 'sport', 'digital'], required: true }, 
+}, {timestamps: true, toJSON: { virtuals: true } })
+
+TowerEventSchema.virtual('creator', {
+    localField: 'creatorId',
+    ref: 'Account',
+    foreignField: '_id',
+    justOne: true
+})
