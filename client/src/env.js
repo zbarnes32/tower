536e6f7dc878120c46a01fc0f334d264a679ff9e
Zach Barnes <zachary.barnes.32@gmail.com>
2024-07-23 15:47:38 -0600

    Got auth0 setup and updated env.js and .env

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-ptn3m0iz4hu4hb7s.us.auth0.com'
+export const clientId = '0mkXC8Wka5lcgZ8bsMcp9Z1DU6KsHRCj'
+export const audience = 'https://gregslistAPI.com'
