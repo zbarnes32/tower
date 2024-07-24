83b2089fc156c48ce789eaec732cc44035ea41cc
Zach Barnes <zachary.barnes.32@gmail.com>
2024-07-24 17:28:38 -0600

    Stopped server. Need to restart

---
+++ client/src/components/CreateEventForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+
+
+const towerEventData = ref({
+   name: '',
+   description: '',
+   startDate: '',
+   location: '',
+    type: '',
+})
+</script>
+
+
+<template>
+<form>
+
+</form>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
